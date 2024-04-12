78faa7a2c82fcc4c5585f8594ee116c041b65e03
juberti <justin@fixie.ai>
2024-04-12 14:52:19 -0700

    add webapp, rework formatting

---
+++ app.py
@@ -0,0 +1,22 @@
+import fastapi
+
+import llm_benchmark_suite
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+async def root():
+    return fastapi.RedirectResponse(url="https://thefastest.ai")
+
+
+@app.route("/bench", methods=["GET", "POST"])
+async def bench(req: fastapi.Request):
+    text, content_type = await llm_benchmark_suite.run(**req.query_params)
+    return fastapi.Response(content=text, media_type=content_type)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # Run the app with uvicorn on port 8000
