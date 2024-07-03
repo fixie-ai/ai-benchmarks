be86aa75b4ef276d3c2e04ca16870d58b99a2f77
juberti <justin@fixie.ai>
2024-07-03 16:14:41 -0700

    add baseten ultravox

--- llm_benchmark_suite.py
+++ llm_benchmark_suite.py
@@ -360,7 +360,26 @@ def _image_models():
     ]
 
 
-def _av_models():
+def _audio_models():
+    return [
+        # _Llm(GPT_4O), doesn't support audio yet
+        # _Llm("gemini-1.5-pro-preview-0514"),
+        # _Llm("gemini-1.5-flash-preview-0514"),
+        _Llm(
+            "fixie-ai/ultravox-v0.2",
+            base_url="https://ultravox.api.fixie.ai/v1",
+            api_key=os.getenv("ULTRAVOX_API_KEY"),
+        ),
+        _Llm(
+            "fixie-ai/ultravox-v0.2",
+            "baseten.co/ultravox-v0.2",
+            base_url="https://bridge.baseten.co/5wovovzq/direct/v1",
+            api_key=os.getenv("BASETEN_API_KEY"),
+        ),
+    ]
+
+
+def _video_models():
     return [
         # _Llm(GPT_4O),
         _Llm("gemini-1.5-pro-preview-0514"),
